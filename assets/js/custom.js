2b4dd36bfcf9e5869b71ee1b9069f93d6ada2641
aaronllcam <aaronllcam@gmail.com>
2020-10-28 12:33:18 +0100

    [ADD] desktop view END

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+/* custom */
+$(document).ready(function(){
+    $('.slider-plug').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true
+    });
+});
